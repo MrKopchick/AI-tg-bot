8f412f9b06d121f4f16cf1b4e073ad25039a422a
Illya Koroliuk <145700600+MrKopchick@users.noreply.github.com>
2026-03-27 22:25:32 +0200

    fix: export token after checking

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,8 +1,8 @@
 import "dotenv/config";
 import { BotErrorMessage } from "../lib/enums/enums";
 
-export const BOT_TOKEN = process.env.BOT_TOKEN;
-
-if (!BOT_TOKEN) {
-  throw new Error(BotErrorMessage.BOT_TOKEN_NOT_FOUND);
+if (!process.env.BOT_TOKEN) {
+    throw new Error(BotErrorMessage.BOT_TOKEN_NOT_FOUND);
 }
+
+export const BOT_TOKEN = process.env.BOT_TOKEN; 
